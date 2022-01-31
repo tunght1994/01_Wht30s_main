dfcbed19e8fd1d36bcd7dccf470a19c5ce5e5464
tunght1994 <thanhtung@21g.in>
2022-01-31 16:21:52 +0700

    'thanhtung'

--- src/components/video-wrap/Video.js
+++ src/components/video-wrap/Video.js
@@ -16,21 +16,21 @@ const Video = () => {
     return (
         <div className='video'>
             <div className="video-header">
-                <h1>Happened’s issue</h1>
-                <p className='video-header-desc'>모바일에 최적화된 비디오를 단 몇 분 만에 제작해보세요. 디자이너가 아니더라도,  모든 팀에서
-                    다듬을 수 있습니다. 브랜드의 성공을 위한 첫 걸음을  내딛어보세요. 비디오를 단 몇 분 만에 제작해보세요.</p>
+                <h1>what happened</h1>
+                <p className='video-header-desc'>How to create mobile-optimized videos in minutes. Not a designer,
+                    every team makes a lot of videos Can be trimmed. Take the first </p>
                 <Button />
             </div>
             <div className="container">
                 <img src={videoImg} alt="" className='video-img' />
-                <img src={videoObject1} alt="videoObject1" className='videoObject1'/>
-                <img src={videoObject3} alt="videoObject3" className='videoObject3'/>
-                <img src={videoObject4} alt="videoObject4" className='videoObject4'/>
-                <img src={videoObject5} alt="videoObject5" className='videoObject5'/>
-                <img src={videoObject6} alt="videoObject6" className='videoObject6'/>
-                <img src={videoObject7} alt="videoObject7" className='videoObject7'/>
-                <img src={videoObject8} alt="videoObject8" className='videoObject8'/>
-                <img src={videoObject9} alt="videoObject9" className='videoObject9'/>
+                <img src={videoObject1} alt="videoObject1" className='videoObject1' />
+                <img src={videoObject3} alt="videoObject3" className='videoObject3' />
+                <img src={videoObject4} alt="videoObject4" className='videoObject4' />
+                <img src={videoObject5} alt="videoObject5" className='videoObject5' />
+                <img src={videoObject6} alt="videoObject6" className='videoObject6' />
+                <img src={videoObject7} alt="videoObject7" className='videoObject7' />
+                <img src={videoObject8} alt="videoObject8" className='videoObject8' />
+                <img src={videoObject9} alt="videoObject9" className='videoObject9' />
             </div>
         </div>
     );
